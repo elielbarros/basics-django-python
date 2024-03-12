0f9627085aa68158a037d9aefd1880e3e099b24e
Eliel Silva <elielbarros.silva@gmail.com>
2024-03-12 20:16:58 -0300

    commit aula 432

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from blog import views
 
+# It is also possible to create a namespace to each url defining app_name.
+# So it will be necessary to implement it inside html references like
+# blog:example
+app_name = 'blog'
+
 # Here inside blog url it is not necessary to write blog/ because it was
 # already defined using the method inside() in project/urls.py
-
+# Name parameter is configured to be used inside html as an url linking
 urlpatterns = [
-    path('', views.blog),
-    path('example/', views.example)
+    path('', views.blog, name='home'),
+    path('example/', views.example, name='example')
 ]
--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from home import views as home_views
 
 
+# It is also possible to create a namespace to each url defining app_name.
+# So it will be necessary to implement it inside html references like
+# blog:example
+app_name = 'home'
+
+# Name parameter is configured to be used inside html as an url linking
 urlpatterns = [
-    path('', home_views.home_view)
+    path('', home_views.home_view, name='home')
 ]
